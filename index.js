8241becb9997660b0a6ee773dc06edcb60c26f4a
kanitsharma <kanitshrm09@gmail.com>
2018-10-19 17:12:15 -0400

    added cots

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 mongoose.connect(
@@ -15,7 +16,7 @@ mongoose.connection.once('open', () => {
     console.log('connected')
 })
 
-
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
